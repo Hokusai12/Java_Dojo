ebb7062ca829fc7ed31e4a759813d99423a63056
Hokusai12 <hokusai2003@gmail.com>
2023-03-05 15:31:29 -0600

    Revert "first commit"
    This reverts commit 24c29b27570dbccd330db2a6c8fb4dbab2aa97d3.

--- Fundamentals/FirstJavaProgram/FirstJavaProgram.java
+++
@@ -1,7 +0,0 @@
-public class FirstJavaProgram {
-    public static void main(String[] args) {
-        System.out.println("My name is Ian Hearne");
-        System.out.println("I am 20 years old");
-        System.out.println("I live in Bentonville, AR");
-    }
-}
--- OOP/.metadata/.plugins/org.eclipse.core.resources/.history/44/70e0ffc589b1001d1f5189d1f9b4bb2e
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-
-public class Order {
-	String name;
-	double total;
-	boolean ready;
-	ArrayList<Item> items;
-}
